ca3e72ce31dc96aca5141a9a9d1bc5d6d8ddbafb
John Safwat <106314101+john-safwat@users.noreply.github.com>
2024-02-16 16:45:49 +0200

    add fragments in home screen
    + Modify activity_home.xml
    + Add content_home.xml
    + Modify fragment_settings.xml
    + Modify fragment_task_list.xml
    + Modify HomeActivity.kt

--- app/src/main/java/com/john/todoandroid/Home/HomeActivity.kt
+++ app/src/main/java/com/john/todoandroid/Home/HomeActivity.kt
@@ -2,17 +2,46 @@ package com.john.todoandroid.Home
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import com.john.todoandroid.Home.Settings.SettingsFragment
+import com.john.todoandroid.Home.Tasks.TasksListFragment
+import com.john.todoandroid.R
 import com.john.todoandroid.databinding.ActivityHomeBinding
 
-class HomeActivity : AppCompatActivity(){
-    lateinit var homeBinding: ActivityHomeBinding
+class HomeActivity : AppCompatActivity() {
+    private lateinit var homeBinding: ActivityHomeBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         initView()
+        setSelectedFragment()
     }
+
 
     private fun initView() {
         homeBinding = ActivityHomeBinding.inflate(layoutInflater)
         setContentView(homeBinding.root)
     }
+
+    private fun setSelectedFragment() {
+        homeBinding.bottomNavigationView.setOnItemSelectedListener {
+            pushFragment(
+                when (it.itemId) {
+                    R.id.navigation_tasks -> {
+                        TasksListFragment()
+                    }
+                    R.id.navigation_settings -> {
+                        SettingsFragment()
+                    }
+                    else -> TasksListFragment()
+                }
+            )
+            true
+        }
+        homeBinding.bottomNavigationView.selectedItemId = R.id.navigation_tasks
+    }
+
+    private fun pushFragment(fragment: Fragment) {
+        supportFragmentManager.beginTransaction().replace(R.id.content_fragment, fragment).commit()
+    }
+
 }
